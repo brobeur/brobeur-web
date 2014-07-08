3a3c2aa2fa50ced7b3f6c9d86945eaf87e9f52dd
wcmckee <will@artcontrol.me>
2014-07-09 09:25:37 +1200

    notebook

---
+++ imgsort.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# Adds images in imgs folder into index.html 
+
+# <markdowncell>
+
+# Make a static blog site out of all the ipython notebook repos on github.com/wcmckee. Saves notebooks as rst and then converts the files to static blog. 
+# Get Nikola working
+# Test centos7
+
+# <codecell>
+
+ls
+
+# <codecell>
+
+import bs4
+
+# <codecell>
+
+lizim = 
+
+# <codecell>
+
+import os
+
+# <codecell>
+
+lisim = os.listdir('/home/pi/brobeur-web/imgs')
+
+# <codecell>
+
+lisim
+
+# <codecell>
+
+filwan = []
+
+# <codecell>
+
+dig cean ba4c053b67db73f90a2eff2fabcddb5f13812689334d8ea87d8b9e4d5e50e0fb
+
+# <codecell>
+
+for im in lisim:
+    if 'png' or 'jpg' in im:
+        filwan.append(im)
+
+# <codecell>
+
+filwan
+
+# <codecell>
+
+
